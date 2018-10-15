30b59362e19664e0df7c3289de8a1d6875750527
aiverson1011 <ajohnson764@gmail.com>
2018-10-15 16:48:20 -0700

    Add class readme and Examples

---
+++ Class06-Interfaces/Demo/OOP_Examples/OOP_Examples/Program.cs
@@ -0,0 +1,18 @@
+﻿using OOP_Examples.Classes;
+using System;
+
+namespace OOP_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            OlderTwentyOne oto = new OlderTwentyOne();
+
+			Dotnet dn = new Dotnet();
+		
+        }
+    }
+}
