3cd20c3c2237c4d52dafb0174eb8431000f997c5
mollyfish <mollyfish@users.noreply.github.com>
2018-10-19 17:32:23 -0700

    Stack and queue demo code for class 10

---
+++ Class10-Recursion-Review/StackQueueDemo/StackQueueDemo/Classes/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueueDemo.Classes
+{
+	public class Stack
+	{
+		public Node Top { get; set; }
+
+		public Stack(Node node)
+		{
+			Top = node;
+		}
+
+		//Peek
+		public Node Peek()
+		{
+			// this is a great place for a try/catch (if you want)
+			return Top;
+		}
+
+		//Pop
+		public Node Pop()
+		{
+			Node temp = Peek();
+			Top = Top.Next;
+			temp.Next = null;
+			return temp;
+		}
+		//Push
+		public void Push(Node node)
+		{
+			node.Next = Top;
+			Top = node;
+		}
+	}
+}
