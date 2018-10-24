61e09b1d2c7b6d962a3daea867191de5563d50e6
aiverson1011 <ajohnson764@gmail.com>
2018-10-24 15:47:08 -0700

    Basic SQL Server database and MVC app with zero tables

---
+++ Class13-EntityFramework/Demo/ClassDemoStudentEnrollment/ClassDemoStudentEnrollment/Data/EnrollmentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassDemoStudentEnrollment.Data
+{
+	public class EnrollmentDbContext : DbContext
+	{
+		public EnrollmentDbContext(DbContextOptions<EnrollmentDbContext> options) : base(options)
+		{
+
+		}
+	}
+}
