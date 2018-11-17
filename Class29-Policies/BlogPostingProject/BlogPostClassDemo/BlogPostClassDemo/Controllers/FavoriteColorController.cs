66c6151ff8ec9be31633f6dc89f482daf4e4dff6
aiverson1011 <ajohnson764@gmail.com>
2018-11-17 14:25:43 -0800

    Add Class 29 Demo

---
+++ Class29-Policies/BlogPostingProject/BlogPostClassDemo/BlogPostClassDemo/Controllers/FavoriteColorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPostClassDemo.Controllers
+{
+	[Authorize(Policy ="FavoriteColor")]
+    public class FavoriteColorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
