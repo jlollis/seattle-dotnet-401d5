9a43ccd8898666bbff417c5b9a74d995f01cca90
aiverson1011 <ajohnson764@gmail.com>
2018-11-19 19:57:46 -0800

    adding class demo code

---
+++ Class31-ViewComponents/Demo/BlogPostClassDemo/BlogPostClassDemo/Components/TopArticles.cs
@@ -0,0 +1,33 @@
+﻿using BlogPostClassDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPostClassDemo.Components
+{
+	
+	public class TopArticles : ViewComponent
+	{
+		private BlogDbContext _context;
+
+		public TopArticles(BlogDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(int number)
+		{
+			var blogs = _context.Blogs.OrderByDescending(n => n.ID)
+						.Take(number)
+						.ToList();
+
+			return View(blogs);
+
+		}
+
+
+
+	}
+}
